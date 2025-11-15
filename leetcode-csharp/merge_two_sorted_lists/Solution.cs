bc075854263ca3b8b0b35342b4021dbcf997196e
Dmitrii Petrov <mrkrivorotoff@gmail.com>
2025-11-15 19:26:16 +0000

    [21. Merge Two Sorted Lists] Add alternative implementation

--- leetcode-csharp/merge_two_sorted_lists/Solution.cs
+++ leetcode-csharp/merge_two_sorted_lists/Solution.cs
@@ -2,17 +2,20 @@ namespace leetcode_csharp.merge_two_sorted_lists;
 
 public class Solution
 {
-    public ListNode? MergeTwoLists(ListNode? list1, ListNode? list2)
+    public ListNode? MergeTwoLists(ListNode? list1, ListNode? list2) =>
+        MergeTwoListsToNewList(list1, list2);
+
+    private static ListNode? MergeTwoListsByChangingNodes(ListNode? list1, ListNode? list2)
     {
         var rootNode = GetCandidateToAppend(list1, list2);
-        if (rootNode == null) return null;
+        if (rootNode is null) return null;
 
         var currentNode = rootNode;
         var candidateToPostpone = ReferenceEquals(rootNode, list1) ? list2 : list1;
         while (true)
         {
             var candidateToAppend = GetCandidateToAppend(currentNode.next, candidateToPostpone);
-            if (candidateToAppend == null) break;
+            if (candidateToAppend is null) break;
             candidateToPostpone = ReferenceEquals(candidateToAppend, currentNode.next)
                 ? candidateToPostpone
                 : currentNode.next;
@@ -23,10 +26,31 @@ public class Solution
         return rootNode;
     }
 
+    private static ListNode? MergeTwoListsToNewList(ListNode? list1, ListNode? list2)
+    {
+        var rootNode = GetCandidateToAppend(list1, list2);
+        if (rootNode == null) return null;
+        var resultRootNode = new ListNode(rootNode.val);
+
+        var resultNode = resultRootNode;
+        var node1 = rootNode.next;
+        var node2 = ReferenceEquals(rootNode, list1) ? list2 : list1;
+        while (true) {
+            var candidate = GetCandidateToAppend(node1, node2);
+            if (candidate == null) break;
+            if (ReferenceEquals(node1, candidate)) node1 = node1.next;
+            else node2 = node2!.next;
+            resultNode.next = new ListNode(candidate.val);
+            resultNode = resultNode.next;
+        }
+
+        return resultRootNode;
+    }
+
     private static ListNode? GetCandidateToAppend(ListNode? node1, ListNode? node2)
     {
-        if (node1 == null) return node2;
-        if (node2 == null) return node1;
+        if (node1 is null) return node2;
+        if (node2 is null) return node1;
         return node1.val < node2.val ? node1 : node2;
     }
 }
