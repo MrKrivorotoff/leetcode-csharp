0a5f1531df6146041524d422ae63a18796b1daa5
Dmitrii Petrov <mrkrivorotoff@gmail.com>
2025-11-21 15:36:44 +0000

    [2154. Keep Multiplying Found Values by Two] Add alternative implementation

--- leetcode-csharp/keep_multiplying_found_values_by_two/Solution.cs
+++ leetcode-csharp/keep_multiplying_found_values_by_two/Solution.cs
@@ -2,11 +2,23 @@ namespace leetcode_csharp.keep_multiplying_found_values_by_two;
 
 public class Solution
 {
-    public int FindFinalValue(int[] nums, int original)
+    public int FindFinalValue(int[] nums, int original) =>
+        FindFinalValueUsingHashSet(nums, original);
+
+    private static int FindFinalValueUsingHashSet(int[] nums, int original)
     {
         var setOfNums = new HashSet<int>(nums);
         while (setOfNums.Contains(original))
             original <<= 1;
+        return original;
+    }
+
+    private static int FindFinalValueUsingSorting(int[] nums, int original)
+    {
+        Array.Sort(nums);
+        foreach (var num in nums)
+            if (original == num)
+                original <<= 1;
         return original;
     }
 }
